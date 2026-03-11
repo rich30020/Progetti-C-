c720f0bbc44db86d21700652b18c519e28783349
Riccardo Mestre <riccardomestre17@gmail.com>
2026-03-11 16:34:31 +0100

    Add files via upload

---
+++ Carrello_Acquisti/Carrello_Acquisti/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Input da tastiera");
+
+        // dichiarazione variabili
+        string? NomeStudente;
+        double primoVoto;
+        double secondoVoto;
+        double terzoVoto;
+
+        try
+        {
+            // input nome studente
+            Console.Write("Inserisci il nome dello studente: ");
+            NomeStudente = Console.ReadLine();
+
+            // input voti
+            Console.Write("Inserisci il primo voto: ");
+            primoVoto = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Inserisci il secondo voto: ");
+            secondoVoto = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Inserisci il terzo voto: ");
+            terzoVoto = Convert.ToDouble(Console.ReadLine());
+
+            // calcolo media
+            double media = (primoVoto + secondoVoto + terzoVoto) / 3;
+
+            // output
+            Console.WriteLine("Studente: " + NomeStudente);
+            Console.WriteLine("Media dello studente: " + media);
+        }
+        catch (Exception exc)
+        {
+            // gestione errore
+            Console.WriteLine("Eccezione: " + exc.Message);
+        }
+        finally
+        {
+            // eseguito sempre
+            Console.WriteLine("Fine del programma");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ Conversione_Euro/ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Input da tastiera");
+
+        // dichiarazione variabili
+        string? importoInEuroInput;
+        double importoInEuro;
+
+        try
+        {
+            // input utente
+            Console.Write("Inserisci l'importo in euro: ");
+            importoInEuroInput = Console.ReadLine();
+
+            // conversione da string a double
+            importoInEuro = Convert.ToDouble(importoInEuroInput);
+
+            // tassi di cambio
+            double dollariUsa = importoInEuro * 1.09;
+            double sterline = importoInEuro * 0.85;
+            double yen = importoInEuro * 161.5;
+
+            // output
+            Console.WriteLine("Conversione valuta:");
+            Console.WriteLine("Dollari USA: " + dollariUsa);
+            Console.WriteLine("Sterline: " + sterline);
+            Console.WriteLine("Yen: " + yen);
+        }
+        catch (Exception exc)
+        {
+            // gestione errore
+            Console.WriteLine("Eccezione: " + exc.Message);
+        }
+        finally
+        {
+            // eseguito sempre
+            Console.WriteLine("Fine del programma");
+        }
+
+        Console.ReadLine();
+    }
+}
